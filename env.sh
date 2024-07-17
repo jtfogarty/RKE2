d8540faea96fd357461cb9ab6a2254977582f8cc
jtfogarty <jeff@fogarty.org>
2024-07-17 12:46:52 -0500

    letsEncrypt

--- env.sh
+++ env.sh
@@ -1,3 +1,5 @@
 export RANCHER_HOSTNAME=rancher.docure.ai
 export RANCHER_BOOTSTRAP_PASSWORD=your_secure_password_here
 export LETSENCRYPT_EMAIL=jeff@fogarty.org
+export NAMECHEAP_API_KEY=
+export NAMECHEAP_USERNAME=
